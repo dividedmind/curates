b7139c7645f0fd7c4c9ca1f9b7d5f28aa451f745
Rafał Rzepecki <divided.mind@gmail.com>
2013-02-13 15:22:33 +0100

    Rails boilerplate.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curates::Application.config.secret_token = 'd0b60424a91cc28cd68b6cfbda27fc12e13c2412acba3ed5c3a4c74e8c23e6c05d9898d0156e2528202246e1ba6db45a6cda5fb858daab66852bae872386797c'
